4188237b469980c5eea41c4c04ec5bd4ac115767
Kazus <120459752+GTAlex1@users.noreply.github.com>
2025-11-18 14:17:41 +0100

    cambios

---
+++ actividad_8.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad 8</title>
+</head>
+<body>
+    <?php
+        $nota = 8;
+        $color = "";
+        if ($nota <=4) {
+            $color = "red";
+        }else if ($nota <=6) {
+            $color = "yellow";
+        }else if ($nota <=8) {
+            $color = "green";
+        }else {
+
+        } $color = "blue"
+
+    ?>
+</body>
+</html>
