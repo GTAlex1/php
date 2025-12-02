98a0c39c424ec1a6f29ef0d7008b84a85720147a
Kazus <120459752+GTAlex1@users.noreply.github.com>
2025-12-02 10:53:16 +0100

    Reorganizar en distintos grupos

--- f/index.php
+++ f/index.php
@@ -1,6 +1,6 @@
 <?php
 // Buscar archivos tipo a1.php, a2.php...
-$archivos = glob("a*.php"); 
+$archivos = glob("f/a*.php"); 
 
 // Ordenar por número (a1, a2, a3...)
 usort($archivos, function($a, $b) {
