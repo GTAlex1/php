cb71f3ce8d343fe35468d83a0bca3a271aaf3fdb
Kazus <120459752+GTAlex1@users.noreply.github.com>
2025-12-05 13:16:59 +0100

    a

--- p/a2.php
+++ p/a2.php
@@ -1,10 +1,10 @@
 <?php
     declare(strict_types=1);
-    const LENGUAGE = "PHP 8.2";
-    function presentacion(string $nombre): string; {
-        return "Hola, $saludo, bienvenido a mi primer script de $LENGUAGE";
+    const LENGUAJE = "PHP 8.2";
+    function presentacion(string $nombre): string {
+        return "Hola, " . $nombre . ", bienvenido a mi primer script de " . LENGUAJE;
     }
 
-    echo presentacion(Kazus);
+    echo presentacion("Kazus");
     echo PHP_EOL;
 ?>
---
+++ p/a3.php
@@ -0,0 +1,7 @@
+<?php
+    declare(strict_types=1);
+    $misJuegos = ["Star Rail", "Colorful Stage", "Brawl Stars", "Blue Protocol"];
+
+    echo $misJuegos[1];
+    echo PHP_EOL;
+?>
