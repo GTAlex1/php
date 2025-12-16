3d3c0cc6dd05a5a84b54a2c6c1df57d976473f55
Kazus <120459752+GTAlex1@users.noreply.github.com>
2025-12-16 11:16:29 +0100

    a

--- formularios/a10.php
+++ formularios/a10.php
@@ -1,12 +1,20 @@
 <?php
-if (isset($_COOKIE['rec'])) echo "Bienvenido de nuevo, " . $_COOKIE['rec'];
 if ($_POST) {
     if ($_POST['u'] === "admin" && $_POST['p'] === "123") {
-        echo "Bienvenido " . $_POST['u'];
-        if (isset($_POST['r'])) setcookie('rec', $_POST['u'], time() + 86400);
+        if (isset($_POST['r'])) {
+            // Seteamos la cookie ANTES de cualquier echo
+            setcookie('rec', $_POST['u'], time() + 86400);
+        }
+        $mensaje = "Bienvenido " . $_POST['u'];
+    } else {
+        $mensaje = "Datos incorrectos";
     }
 }
+
+if (isset($_COOKIE['rec'])) echo "Bienvenido de nuevo, " . $_COOKIE['rec'] . "<br>";
+if (isset($mensaje)) echo $mensaje;
 ?>
+
 <form method="post">
     User: <input type="text" name="u">
     Pass: <input type="password" name="p">
